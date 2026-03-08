c1a3218875c266b5bb2ca79e30683482f7acd188
Aryan <gangapuramaryanx1b@gmail.com>
2026-03-08 16:43:35 +0530

    Added new schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -3,9 +3,25 @@ from pydantic import BaseModel
 class RestaurantCreate(BaseModel):
     id :int
     name: str
+    location:str
+    capacity:int
+    rating:int
 class RestaurantResponse(BaseModel):
     id: int
     name: str
+    location:str
+    capacity:int
+    rating:int
+class MenuCreate(BaseModel):
+     item_name: str
+     price: int
+     restaurant_id: int
+class MenuResponse(BaseModel):
+    id: int
+    item_name: str
+    price: int
+    restaurant_id: int
 
-    class Config:
-        orm_mode = True
+
+class Config:
+        rom_attributes = True
