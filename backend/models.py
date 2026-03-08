a7b9bd83f95f44ee8d69d64579f036c18c239930
Aryan <gangapuramaryanx1b@gmail.com>
2026-03-08 16:36:15 +0530

    Modified Tables
    Added a new table called Menu and changed the Restaurant table

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,22 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,ForeignKey
 from database import Base
+from sqlalchemy.orm import relationship
+
+class Menu(Base):
+    __tablename__="menu"
+    id=Column(Integer,primary_key=True)
+    item_name=Column(String)
+    price=Column(Integer)
+    restaurant_id = Column(Integer, ForeignKey("restaurants.id"))
+    restaurant = relationship("Restaurant", back_populates="menu_items")
+
 
 class Restaurant(Base):
     __tablename__ = "restaurants"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-
-
-
+    location=Column(String)
+    capacity=Column(Integer)
+    rating=Column(Integer)
+    menu_items = relationship("Menu", back_populates="restaurant")
